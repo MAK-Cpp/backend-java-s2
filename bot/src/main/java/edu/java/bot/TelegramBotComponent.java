129dbc7d187cd6d8fc7826575a94ddb81ee11dba
Maxim Primakov <spartmenik@gmail.com>
2024-02-14 20:41:13 +0300

    fix try No. 2

--- bot/src/main/java/edu/java/bot/TelegramBotComponent.java
+++ bot/src/main/java/edu/java/bot/TelegramBotComponent.java
@@ -16,6 +16,7 @@ import edu.java.bot.configuration.ApplicationConfig;
 import edu.java.bot.requests.chains.Chains;
 import edu.java.bot.requests.chains.EditMessageTextChains;
 import edu.java.bot.requests.chains.SendMessageChains;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,9 +55,13 @@ public final class TelegramBotComponent extends TelegramBot {
         LOGGER.debug("Finished TelegramBot constructor");
         setUpdatesListener(this::updateListener, this::exceptionHandler);
         LOGGER.debug("Set updateListener & exceptionHandler");
+    }
+
+    @PostConstruct
+    public void init() {
         setCommands(Command.START, Command.HELP, Command.TRACK, Command.UNTRACK, Command.LIST);
         LOGGER.debug("Set setCommands START HELP TRACK UNTRACK LIST");
-        LOGGER.debug("Created bot with token " + config.telegramToken());
+        LOGGER.debug("Created bot with token " + this.getToken());
     }
 
     /*package-private*/ void addUser(long id, User user) {
