c4aff34bf7a6fb63e3415facf1f309293e47c5d1
Maxim Primakov <spartmenik@gmail.com>
2024-03-09 15:09:04 +0300

    written task No. 4

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -2,7 +2,6 @@ package edu.java.scrapper;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -16,12 +15,6 @@ public abstract class IntegrationTest {
             .withUsername("postgres")
             .withPassword("postgres");
         POSTGRES.start();
-
-        runMigrations(POSTGRES);
-    }
-
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
     }
 
     @DynamicPropertySource
